e98241dc78d549f6f0a8cadb96003bf008761527
Łukasz Wieczorek <lukasz.wieczorek@ironin.pl>
2020-10-14 14:35:22 +0200

    Allow all hosts

--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'z(n&q9i%wke#&eo0u^u8^pvbdgf0=y#09d-5sf3gw=_70vj_s)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
